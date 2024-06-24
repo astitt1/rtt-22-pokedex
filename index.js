c27b0a4e1975e96e778d1e4476193792901368df
astitt1 <astitt@perscholas.org>
2024-06-24 15:28:17 -0400

    created pokecard for each pokemon

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
-const poke_container = document.getElementById('poke-container')
+const poke_container = document.getElementById("poke-container");
 // console.log(poke_container)
 const pokemon_count = 150;
 
-
 const fetchPokemons = async () => {
-    for(let id = 1; id <= pokemon_count; id++){
-        //we are going to call a function that makes the api call
-        await getPokemon(id) // 1, 2, 3, 4....
-    }
-}
+  for (let id = 1; id <= pokemon_count; id++) {
+    //we are going to call a function that makes the api call
+    await getPokemon(id); // 1, 2, 3, 4....
+  }
+};
 
 const getPokemon = async (id) => {
-    try {
-        //api call to the pokeapi
-        const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-        const response = await fetch(url)
-        const pokemon = await response.json()
-        console.log(pokemon)
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    //api call to the pokeapi
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const response = await fetch(url);
+    const pokemon = await response.json();
+    // console.log(pokemon)
+    createPokemonCard(pokemon); //{}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-fetchPokemons()
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+//   console.log(pokemonEl)
+
+  const pokemonInnerHTML = `
+     <div class="img-container">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="">
+            </div>
+            <div class="info">
+                <span class="number">${pokemon.id}</span>
+                <h3 class="name">${pokemon.name}</h3>
+                <small class="type"> Type <span> ${pokemon.types[0].type.name}</span> </small>
+            </div>
+    `;
+    pokemonEl.innerHTML = pokemonInnerHTML
+    poke_container.appendChild(pokemonEl)
+    // console.log(pokemonEl)
+};
+fetchPokemons();
+
+/** 
+ *  <div class="pokemon">
+            <div class="img-container">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="">
+            </div>
+            <div class="info">
+                <span class="number">#001</span>
+                <h3 class="name">Balbasaur</h3>
+                <small class="type"> Type <span>grass</span> </small>
+            </div>
+        </div>
+ * 
+ * 
+ * 
+ * 
+*/
